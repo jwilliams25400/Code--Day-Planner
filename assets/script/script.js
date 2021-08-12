e446c7264cd5d01a5f0868291b2f52f702ea5565
Jemel Williams <jwilliams25400@yahoo.com>
2021-08-12 19:50:51 -0400

    added bootstrap, jquery and google font and beginner html code

---
+++ assets/script/script.js
@@ -0,0 +1,38 @@
+// create our docuemnt ready function to make sure nothing runs before we load the page
+$(document).ready(function(){
+
+    // function  to listen for click even on the page
+    // grab and save event options
+    //creat variables to save user input and time
+
+    //save to localstorage
+
+    //alert user that items have been saved to local storage
+
+    //creat some kind of timeout value that removes the notification
+
+
+
+    // creat time updater function()
+    //get the current time
+
+    // create  our loop to go over all the time blocks
+
+    // if else statnement for below 3
+    // need a ().removeClass() or add class
+    // check to see if we have passed our time
+
+    //check to see if current hour matches physical hour
+
+    // check to see if the time is in the future
+
+    // retrieve local storage an render items to correct rows
+}
+
+
+
+
+
+
+
+)
