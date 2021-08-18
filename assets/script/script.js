ebf16de2041c58f6f71c3a6bec5bce476072028f
Jemel Williams <jwilliams25400@yahoo.com>
2021-08-18 15:42:05 -0400

    finally got localstorage to work pushing final updates

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,251 +1,203 @@
-var todayEl = $("#today")
-var CurrentDate = moment().format("MMMM Do YYYY");
-var currentHour = moment().format("H")
-var textbox = [
-    $('#comment-area9'),
-    $('#comment-area10'),
-    $('#comment-area11'),
-    $('#comment-area12'),
-    $('#comment-area1'),
-    $('#comment-area2'),
-    $('#comment-area3'),
-    $('#comment-area4'),
-    $('#comment-area5'),
-    
-]
+$(document).ready(function () {
+  var todayEl = $("#today");
+  var currentHour = moment().format("H");
+  var saveBtn = $(".save-btn");
+  var clearEl = $("#clear-planner");
+  var boxesEl = $("#time-boxes");
+  var textboxes = [
+    $("#area9"),
+    $("#area10"),
+    $("#area11"),
+    $("#area12"),
+    $("#area1"),
+    $("#area2"),
+    $("#area3"),
+    $("#area4"),
+    $("#area5"),
+  ];
 
-if(currentHour === "9"){
-    $("#comment-area9").style.backgroundColor = "red"
-    $("#comment-area10").style.backgroundColor = "green"
-    $("#comment-area11").style.backgroundColor = "green"
-    $("#comment-area12").style.backgroundColor = "green"
-    $("#comment-area1").style.backgroundColor = "green"
-    $("#comment-area2").style.backgroundColor = "green"
-    $("#comment-area3").style.backgroundColor = "green"
-    $("#comment-area4").style.backgroundColor = "green"
-    $("#comment-area5").style.backgroundColor = "green"
-}
-else if(currentHour === "10"){
-    $("#comment-area9").style.backgroundColor = "grey"
-    $("#comment-area10").style.backgroundColor = "red"
-    $("#comment-area11").style.backgroundColor = "green"
-    $("#comment-area12").style.backgroundColor = "green"
-    $("#comment-area1").style.backgroundColor = "green"
-    $("#comment-area2").style.backgroundColor = "green"
-    $("#comment-area3").style.backgroundColor = "green"
-    $("#comment-area4").style.backgroundColor = "green"
-    $("#comment-area5").style.backgroundColor = "green"
-}
-else if(currentHour === "11"){
-    $("#comment-area9").style.backgroundColor = "grey"
-    $("#comment-area10").style.backgroundColor = "grey"
-    $("#comment-area11").style.backgroundColor = "red"
-    $("#comment-area12").style.backgroundColor = "green"
-    $("#comment-area1").style.backgroundColor = "green"
-    $("#comment-area2").style.backgroundColor = "green"
-    $("#comment-area3").style.backgroundColor = "green"
-    $("#comment-area4").style.backgroundColor = "green"
-    $("#comment-area5").style.backgroundColor = "green"
-}
-else if(currentHour === "12"){
-    $("#comment-area9").style.backgroundColor = "grey"
-    $("#comment-area10").style.backgroundColor = "grey"
-    $("#comment-area11").style.backgroundColor = "grey"
-    $("#comment-area12").style.backgroundColor = "red"
-    $("#comment-area1").style.backgroundColor = "green"
-    $("#comment-area2").style.backgroundColor = "green"
-    $("#comment-area3").style.backgroundColor = "green"
-    $("#comment-area4").style.backgroundColor = "green"
-    $("#comment-area5").style.backgroundColor = "green"
-}
-else if(currentHour === "1"){
-    $("#comment-area9").style.backgroundColor = "grey"
-    $("#comment-area10").style.backgroundColor = "grey"
-    $("#comment-area11").style.backgroundColor = "grey"
-    $("#comment-area12").style.backgroundColor = "grey"
-    $("#comment-area1").style.backgroundColor = "red"
-    $("#comment-area2").style.backgroundColor = "green"
-    $("#comment-area3").style.backgroundColor = "green"
-    $("#comment-area4").style.backgroundColor = "green"
-    $("#comment-area5").style.backgroundColor = "green"
-}
-else if(currentHour === "2"){
-    $("#comment-area9").style.backgroundColor = "grey"
-    $("#comment-area10").style.backgroundColor = "grey"
-    $("#comment-area11").style.backgroundColor = "grey"
-    $("#comment-area12").style.backgroundColor = "grey"
-    $("#comment-area1").style.backgroundColor = "grey"
-    $("#comment-area2").style.backgroundColor = "red"
-    $("#comment-area3").style.backgroundColor = "green"
-    $("#comment-area4").style.backgroundColor = "green"
-    $("#comment-area5").style.backgroundColor = "green"
-}
-else if(currentHour === "3"){
-    $("#comment-area9").style.backgroundColor = "grey"
-    $("#comment-area10").style.backgroundColor = "grey"
-    $("#comment-area11").style.backgroundColor = "grey"
-    $("#comment-area12").style.backgroundColor = "grey"
-    $("#comment-area1").style.backgroundColor = "grey"
-    $("#comment-area2").style.backgroundColor = "grey"
-    $("#comment-area3").style.backgroundColor = "red"
-    $("#comment-area4").style.backgroundColor = "green"
-    $("#comment-area5").style.backgroundColor = "green"
-}
-else if(currentHour === "4"){
-    $("#comment-area9").style.backgroundColor = "grey"
-    $("#comment-area10").style.backgroundColor = "grey"
-    $("#comment-area11").style.backgroundColor = "grey"
-    $("#comment-area12").style.backgroundColor = "grey"
-    $("#comment-area1").style.backgroundColor = "grey"
-    $("#comment-area2").style.backgroundColor = "grey"
-    $("#comment-area3").style.backgroundColor = "grey"
-    $("#comment-area4").style.backgroundColor = "red"
-    $("#comment-area5").style.backgroundColor = "green"
-}
-else if(currentHour === "5"){
-    $("#comment-area9").style.backgroundColor = "grey"
-    $("#comment-area10").style.backgroundColor = "grey"
-    $("#comment-area11").style.backgroundColor = "grey"
-    $("#comment-area12").style.backgroundColor = "grey"
-    $("#comment-area1").style.backgroundColor = "grey"
-    $("#comment-area2").style.backgroundColor = "grey"
-    $("#comment-area3").style.backgroundColor = "grey"
-    $("#comment-area4").style.backgroundColor = "grey"
-    $("#comment-area5").style.backgroundColor = "red"
-}
+  // set date and time in header
+  function displayDayTime() {
+    var dayAndTimeEL = moment().format("MMM DD, YYYY [at] hh:mm:ss a ");
+    todayEl.text(dayAndTimeEL);
+  }
+  setInterval(displayDayTime, 1000);
 
-// store calender notes
-function newPlan9() {
-    var plan = $(this).prev().val() + " On this day: " + moment().format("dddd, MMMM, Do");
-    localStorage.setitem("plan9", JSON.stringify(newPlan));
-    $$('#comment-area9').val("")
-    $('#comment-area9'). val(JSON.parse(localStorage.plan9))
-}
-$("#save9").click (newPlan9)
-function newPlan10() {
-    var plan = $(this).prev().val() + " On this day: " + moment().format("dddd, MMMM, Do");
-    localStorage.setitem("plan10", JSON.stringify(newPlan));
-    $$('#comment-area10').val("")
-    $('#comment-area10'). val(JSON.parse(localStorage.plan10))
-}
-$("#save10").click (newPlan10)
-function newPlan11() {
-    var plan = $(this).prev().val() + " On this day: " + moment().format("dddd, MMMM, Do");
-    localStorage.setitem("plan11", JSON.stringify(newPlan));
-    $$('#comment-area11').val("")
-    $('#comment-area11'). val(JSON.parse(localStorage.plan11))
-}
-$("#save11").click (newPlan11)
-function newPlan12() {
-    var plan = $(this).prev().val() + " On this day: " + moment().format("dddd, MMMM, Do");
-    localStorage.setitem("plan12", JSON.stringify(newPlan));
-    $$('#comment-area12').val("")
-    $('#comment-area12'). val(JSON.parse(localStorage.plan12))
-}
-$("#save12").click (newPlan12)
-function newPlan1() {
-    var plan = $(this).prev().val() + " On this day: " + moment().format("dddd, MMMM, Do");
-    localStorage.setitem("plan1", JSON.stringify(newPlan));
-    $$('#comment-area1').val("")
-    $('#comment-area1'). val(JSON.parse(localStorage.plan1))
-}
-$("#save1").click (newPlan1)
-function newPlan2() {
-    var plan = $(this).prev().val() + " On this day: " + moment().format("dddd, MMMM, Do");
-    localStorage.setitem("plan2", JSON.stringify(newPlan));
-    $$('#comment-area2').val("")
-    $('#comment-area2'). val(JSON.parse(localStorage.plan2))
-}
-$("#save2").click (newPlan2)
-function newPlan3() {
-    var plan = $(this).prev().val() + " On this day: " + moment().format("dddd, MMMM, Do");
-    localStorage.setitem("plan3", JSON.stringify(newPlan));
-    $$('#comment-area3').val("")
-    $('#comment-area3'). val(JSON.parse(localStorage.plan3))
-}
-$("#save3").click (newPlan3)
-function newPlan4() {
-    var plan = $(this).prev().val() + " On this day: " + moment().format("dddd, MMMM, Do");
-    localStorage.setitem("plan1", JSON.stringify(newPlan));
-    $$('#comment-area4').val("")
-    $('#comment-area4'). val(JSON.parse(localStorage.plan4))
-}
-$("#save4").click (newPlan4)
-function newPlan5() {
-    var plan = $(this).prev().val() + " On this day: " + moment().format("dddd, MMMM, Do");
-    localStorage.setitem("plan5", JSON.stringify(newPlan));
-    $$('#comment-area5').val("")
-    $('#comment-area5'). val(JSON.parse(localStorage.plan5))
-}
-$("#save5").click (newPlan5)
+  // set color for various hours throughout day
 
-function deletePlans() {
-    for (var i = 0; i <)
-}
-
-
-
-
-
-
-
-
-
-
-
-function showTime() {
-    var currentTd =  moment().format('MMM DD, YYYY [at] hh:mm:ss a');
-     actDayTimeEl.text(currentTd);
+  if (currentHour === "9") {
+    $("#area9").css("background-color", "red");
+    $("#area10").css("background-color", "green");
+    $("#area11").css("background-color", "green");
+    $("#area12").css("background-color", "green");
+    $("#area1").css("background-color", "green");
+    $("#area2").css("background-color", "green");
+    $("#area3").css("background-color", "green");
+    $("#area4").css("background-color", "green");
+    $("#area5").css("background-color", "green");
+  } else if (currentHour === "10") {
+    $("#area9").css("background-color", "grey");
+    $("#area10").css("background-color", "red");
+    $("#area11").css("background-color", "green");
+    $("#area12").css("background-color", "green");
+    $("#area1").css("background-color", "green");
+    $("#area2").css("background-color", "green");
+    $("#area3").css("background-color", "green");
+    $("#area4").css("background-color", "green");
+    $("#area5").css("background-color", "green");
+  } else if (currentHour === "11") {
+    $("#area9").css("background-color", "grey");
+    $("#area10").css("background-color", "grey");
+    $("#area11").css("background-color", "red");
+    $("#area12").css("background-color", "green");
+    $("#area1").css("background-color", "green");
+    $("#area2").css("background-color", "green");
+    $("#area3").css("background-color", "green");
+    $("#area4").css("background-color", "green");
+    $("#area5").css("background-color", "green");
+  } else if (currentHour === "12") {
+    $("#area9").css("background-color", "grey");
+    $("#area10").css("background-color", "grey");
+    $("#area11").css("background-color", "grey");
+    $("#area12").css("background-color", "red");
+    $("#area1").css("background-color", "green");
+    $("#area2").css("background-color", "green");
+    $("#area3").css("background-color", "green");
+    $("#area4").css("background-color", "green");
+    $("#area5").css("background-color", "green");
+  } else if (currentHour === "13") {
+    $("#area9").css("background-color", "grey");
+    $("#area10").css("background-color", "grey");
+    $("#area11").css("background-color", "grey");
+    $("#area12").css("background-color", "grey");
+    $("#area1").css("background-color", "red");
+    $("#area2").css("background-color", "green");
+    $("#area3").css("background-color", "green");
+    $("#area4").css("background-color", "green");
+    $("#area5").css("background-color", "green");
+  } else if (currentHour === "14") {
+    $("#area9").css("background-color", "grey");
+    $("#area10").css("background-color", "grey");
+    $("#area11").css("background-color", "grey");
+    $("#area12").css("background-color", "grey");
+    $("#area1").css("background-color", "grey");
+    $("#area2").css("background-color", "red");
+    $("#area3").css("background-color", "green");
+    $("#area4").css("background-color", "green");
+    $("#area5").css("background-color", "green");
+  } else if (currentHour === "15") {
+    $("#area9").css("background-color", "grey");
+    $("#area10").css("background-color", "grey");
+    $("#area11").css("background-color", "grey");
+    $("#area12").css("background-color", "grey");
+    $("#area1").css("background-color", "grey");
+    $("#area2").css("background-color", "grey");
+    $("#area3").css("background-color", "red");
+    $("#area4").css("background-color", "green");
+    $("#area5").css("background-color", "green");
+  } else if (currentHour === "16") {
+    $("#area9").css("background-color", "grey");
+    $("#area10").css("background-color", "grey");
+    $("#area11").css("background-color", "grey");
+    $("#area12").css("background-color", "grey");
+    $("#area1").css("background-color", "grey");
+    $("#area2").css("background-color", "grey");
+    $("#area3").css("background-color", "grey");
+    $("#area4").css("background-color", "red");
+    $("#area5").css("background-color", "green");
+  } else if (currentHour === "17") {
+    $("#area9").css("background-color", "grey");
+    $("#area10").css("background-color", "grey");
+    $("#area11").css("background-color", "grey");
+    $("#area12").css("background-color", "grey");
+    $("#area1").css("background-color", "grey");
+    $("#area2").css("background-color", "grey");
+    $("#area3").css("background-color", "grey");
+    $("#area4").css("background-color", "grey");
+    $("#area5").css("background-color", "red");
+  }
+  else {
+    $("#area9").css("background-color", "grey");
+    $("#area10").css("background-color", "grey");
+    $("#area11").css("background-color", "grey");
+    $("#area12").css("background-color", "grey");
+    $("#area1").css("background-color", "grey");
+    $("#area2").css("background-color", "grey");
+    $("#area3").css("background-color", "grey");
+    $("#area4").css("background-color", "grey");
+    $("#area5").css("background-color", "grey");
   }
 
+  // set local storage to empty
+  var dayPlannerNoteEl = JSON.parse(
+    localStorage.getItem("dayPlannerNoteEl")
+  ) || {
+    area9: "",
+    area10: "",
+    area11: "",
+    area12: "",
+    area1: "",
+    area2: "",
+    area3: "",
+    area4: "",
+    area5: "",
+  };
 
-// create our docuemnt ready function to make sure nothing runs before we load the page
-$(document).ready(function(){
-    $(".save-btn").on("click", function(){
-        var notes = $(this).sibling(".comment").setInterval();
-        var time = $(this).parent().attr("id");
-        localStorage.setitem(time, value);
-    });
+  localStorage.setItem("dayPlannerNoteEl", JSON.stringify(dayPlannerNoteEl));
+  $("#area9").val(dayPlannerNoteEl.area9);
+  $("#area10").val(dayPlannerNoteEl.area10);
+  $("#area11").val(dayPlannerNoteEl.area11);
+  $("#area12").val(dayPlannerNoteEl.area12);
+  $("#area1").val(dayPlannerNoteEl.area1);
+  $("#area2").val(dayPlannerNoteEl.area2);
+  $("#area3").val(dayPlannerNoteEl.area3);
+  $("#area4").val(dayPlannerNoteEl.area4);
+  $("#area5").val(dayPlannerNoteEl.area5);
 
-    function updateTheHour() {
-        var currentHour = moment().hours();
+  // store calender notes
 
-        $
-    }
+  // clear planner
+  clearEl.on("click", function () {
+    boxesEl.empty();
+  });
+  // function clearPlanner()
+  // $("#clear-planner").click(() => {
+  //   localStorage.clear();
+  //   location.reload();
+  // });
 
+  // create our document ready function to make sure nothing runs before we load the page
+  $(".save-btn").on("click", function () {
+    var dayPlannerNoteEl = JSON.parse(localStorage.getItem("dayPlannerNoteEl"));
+    var commentEl = $(this).siblings(".comment").val();
 
+    var timeEl = $(this).siblings(".comment").attr("id");
+    dayPlannerNoteEl[timeEl] = commentEl;
+    localStorage.setItem("dayPlannerNoteEl", JSON.stringify(dayPlannerNoteEl));
+  });
+});
 
+// function  to listen for click even on the page
+// grab and save event options
+//creat variables to save user input and time
 
-}
+//save to localstorage
 
-    
-    // function  to listen for click even on the page
-    // grab and save event options
-    //creat variables to save user input and time
+//alert user that items have been saved to local storage
 
-    //save to localstorage
+//creat some kind of timeout value that removes the notification
 
-    //alert user that items have been saved to local storage
+// creat time updater function()
+//get the current time
 
-    //creat some kind of timeout value that removes the notification
+// create  our loop to go over all the time blocks
 
+// if else statnement for below 3
+// need a ().removeClass() or add class
+// check to see if we have passed our time
 
+//check to see if current hour matches physical hour
 
-    // creat time updater function()
-    //get the current time
+// check to see if the time is in the future
 
-    // create  our loop to go over all the time blocks
-
-    // if else statnement for below 3
-    // need a ().removeClass() or add class
-    // check to see if we have passed our time
-
-    //check to see if current hour matches physical hour
-
-    // check to see if the time is in the future
-
-    // retrieve local storage an render items to correct rows
-}
-
-)
-setInterval(showTime), 1000);
+// retrieve local storage an render items to correct.
